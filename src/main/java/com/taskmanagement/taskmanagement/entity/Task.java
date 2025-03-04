4ea812f2e9782f59074d610d5f8a862ed57a4237
Santhi Kaliyappan <ksanthi88@gmail.com>
2025-03-03 21:23:16 -0600

    first commit

---
+++ src/main/java/com/taskmanagement/taskmanagement/entity/Task.java
@@ -0,0 +1,4 @@
+package com.taskmanagement.taskmanagement.entity;
+
+public class Task {
+}
---
+++ src/main/java/com/taskmanagement/taskmanagement/repository/TaskRepository.java
@@ -0,0 +1,4 @@
+package com.taskmanagement.taskmanagement.repository;
+
+public interface Taskrepository {
+}
